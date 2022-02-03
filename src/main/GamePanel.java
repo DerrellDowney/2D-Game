daaa7924224186c7f03c0576caa8b2f466e1d320
Derrell Downey <derrell.downey@gmail.com>
2022-02-02 23:30:11 -0500

    updated work

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -7,5 +7,11 @@ public class GamePanel extends JPanel{
 	//SCREEN SETTINGS
 	
 	final int originalTileSize = 16; // 16x16 tiles
+	final int scale = 3;
 	
+	final int tileSize = originalTileSize * scale; //48x48 px tiles
+	final int maxScreenCol = 16;
+	final int maxScreenRow = 12; //4:3 ratio
+	final int screenWidth = tileSize * maxScreenCol; // 768px
+	final int screenHeight = tileSize * maxScreenRow; // 576px
 }
